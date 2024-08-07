0dcdfc94375e1bbd057e61dbdb1d46bc25a2f610
Perumalla Hima Chaitanya <118809071+Chaitanyaperumalla@users.noreply.github.com>
2024-08-07 02:38:09 -0700

    Time: 2 ms (83%), Space: 45.4 MB (97.94%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,13 +1,34 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-     int a=0;
-     for(int i=0;i<nums.length;i++)
-        if(nums[i]!=0)
-            nums[a++]=nums[i];
+    //  int a=0;
+    //  for(int i=0;i<nums.length;i++)
+    //     if(nums[i]!=0)
+    //         nums[a++]=nums[i];
      
 
-     for(int i=a;i<nums.length;i++) nums[i]=0;
+    //  for(int i=a;i<nums.length;i++) nums[i]=0;
 
+        int ind = -1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == 0){
+                ind = i;
+                break;
+            }
+        }
+        
+        if(ind == -1) return;
+        
+        int i = ind;
+        int j = ind+1;
+        while(j<nums.length){
+            if(nums[j] != 0){
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                i++;
+            }
+            j++;
+        }
     }
     
 }
