94b69b8dfeaf68d4867feca2295befe451bfb5e0
Perumalla Hima Chaitanya <118809071+Chaitanyaperumalla@users.noreply.github.com>
2024-08-07 23:51:33 -0700

    Time: 311 ms (7.83%), Space: 46.4 MB (9.29%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -3,17 +3,19 @@ class Solution {
         // int n = 0;
         // for(int i:nums) n ^= i;
         // return n;
-        for(int i=0;i<nums.length;i++) {
-            int j =0;
-            for(j=0;j<nums.length;j++) {
-                if(i!=j && nums[i]==nums[j]) {
+       for(int i=0;i<nums.length;i++){
+            boolean found = false;
+            for(int j=0;j<nums.length;j++){
+                if(i!=j && nums[i] == nums[j]){
+                    found = true;
                     break;
                 }
             }
-            if(j==nums.length) return nums[i];
+            if(!found){
+                return nums[i];
+            }
         }
         return 0;
-        
     }
 }
 
