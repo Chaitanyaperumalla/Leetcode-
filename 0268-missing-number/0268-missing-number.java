caaab8af59e9a1b5c72699574c80e8d5bc4b5e11
Perumalla Hima Chaitanya <118809071+Chaitanyaperumalla@users.noreply.github.com>
2024-08-08 21:33:25 -0700

    Time: 0 ms (100%), Space: 45.4 MB (26.14%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,11 +1,16 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length;
-        int total  =   (n * (n+1))/2;
-        int sum  =0;
-        for(int i=0;i< n;i++){
-            sum += nums[i];
-        }
-        return total-sum;
+        // int n = nums.length;
+        // int total  =   (n * (n+1))/2;
+        // int sum  =0;
+        // for(int i=0;i< n;i++){
+        //     sum += nums[i];
+        // }
+        // return total-sum;
+
+        int k=0;
+        int xor = nums.length;
+        for(int i:nums) xor^=i^k++;
+        return xor;
     }
 }
