a8e5e0eaf67c12bae24fb4a985d9eab848924aae
Perumalla Hima Chaitanya <118809071+Chaitanyaperumalla@users.noreply.github.com>
2024-08-08 21:50:33 -0700

    Time: 34 ms (17.89%), Space: 58.2 MB (52.74%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -10,11 +10,16 @@ class Solution {
         // return 0;
 
 
+        // Arrays.sort(nums);
+        // for(int i=0;i<nums.length-1;i++){
+        //     if((nums[i]^nums[i+1]) ==0) return nums[i];
+        // }return 0;
+        
         Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++){
-            if((nums[i]^nums[i+1]) ==0) return nums[i];
-        }return 0;
-        
-        
+        for(int i=0;i<nums.length-1;i++) {
+            if(nums[i]==nums[i+1])
+            return nums[i];
+        }
+        return 0;
     }
 }
