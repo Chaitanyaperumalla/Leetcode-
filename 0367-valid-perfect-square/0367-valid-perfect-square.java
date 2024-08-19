86d93da92433e44826a5556c02560bc2f92166cc
Perumalla Hima Chaitanya <118809071+Chaitanyaperumalla@users.noreply.github.com>
2024-08-18 23:50:38 -0700

    Time: 0 ms (100%), Space: 40.3 MB (48.54%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,5 +1,14 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        return (int)Math.sqrt(num)* (int)Math.sqrt(num) == num;
+        if(num<2) return true;
+       int low = 0;
+       int high = num;
+       while(low<=high) {
+        int mid=(low+high)/2;
+        long temp=(long)mid * mid;
+        if(temp==num ) return true;
+        else if(temp >num) high=mid-1;
+        else low=mid+1;
+       } return false;
     }
 }
